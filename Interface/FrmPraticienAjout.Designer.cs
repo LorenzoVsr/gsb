4e8a106d05a982b7b404f10f648447d15c8066ed
Lorenzo <lorenzo.vasseur@saint-remi.net>
2026-04-07 17:01:03 +0200

    07/04

---
+++ Interface/FrmPraticienAjout.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace Interface
+{
+    partial class FrmPraticienAjout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPraticienAjout));
+            panelCentral = new Panel();
+            msgSpecialite = new Label();
+            msgType = new Label();
+            msgEmail = new Label();
+            msgTelephone = new Label();
+            msgVille = new Label();
+            msgRue = new Label();
+            msgPrenom = new Label();
+            msgNom = new Label();
+            BtnAjouter = new Button();
+            Specialite = new Label();
+            cbxType = new ComboBox();
+            cbxSpecialite = new ComboBox();
+            Telephone = new Label();
+            mtbTelephone = new MaskedTextBox();
+            Type = new Label();
+            Email = new Label();
+            Ville = new Label();
+            Rue = new Label();
+            Prenom = new Label();
+            Nom = new Label();
+            txtPrenom = new TextBox();
+            txtEmail = new TextBox();
+            txtVille = new TextBox();
+            txtRue = new TextBox();
+            txtNom = new TextBox();
+            panelCentral.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            lblTitre.Size = new Size(838, 74);
+            // 
+            // panelCentral
+            // 
+            panelCentral.Controls.Add(msgSpecialite);
+            panelCentral.Controls.Add(msgType);
+            panelCentral.Controls.Add(msgEmail);
+            panelCentral.Controls.Add(msgTelephone);
+            panelCentral.Controls.Add(msgVille);
+            panelCentral.Controls.Add(msgRue);
+            panelCentral.Controls.Add(msgPrenom);
+            panelCentral.Controls.Add(msgNom);
+            panelCentral.Controls.Add(BtnAjouter);
+            panelCentral.Controls.Add(Specialite);
+            panelCentral.Controls.Add(cbxType);
+            panelCentral.Controls.Add(cbxSpecialite);
+            panelCentral.Controls.Add(Telephone);
+            panelCentral.Controls.Add(mtbTelephone);
+            panelCentral.Controls.Add(Type);
+            panelCentral.Controls.Add(Email);
+            panelCentral.Controls.Add(Ville);
+            panelCentral.Controls.Add(Rue);
+            panelCentral.Controls.Add(Prenom);
+            panelCentral.Controls.Add(Nom);
+            panelCentral.Controls.Add(txtPrenom);
+            panelCentral.Controls.Add(txtEmail);
+            panelCentral.Controls.Add(txtVille);
+            panelCentral.Controls.Add(txtRue);
+            panelCentral.Controls.Add(txtNom);
+            panelCentral.Location = new Point(0, 101);
+            panelCentral.Name = "panelCentral";
+            panelCentral.Size = new Size(839, 444);
+            panelCentral.TabIndex = 13;
+            // 
+            // msgSpecialite
+            // 
+            msgSpecialite.AutoSize = true;
+            msgSpecialite.ForeColor = Color.Red;
+            msgSpecialite.Location = new Point(413, 358);
+            msgSpecialite.Name = "msgSpecialite";
+            msgSpecialite.Size = new Size(30, 15);
+            msgSpecialite.TabIndex = 25;
+            msgSpecialite.Text = "msg";
+            // 
+            // msgType
+            // 
+            msgType.AutoSize = true;
+            msgType.ForeColor = Color.Red;
+            msgType.Location = new Point(31, 358);
+            msgType.Name = "msgType";
+            msgType.Size = new Size(30, 15);
+            msgType.TabIndex = 24;
+            msgType.Text = "msg";
+            // 
+            // msgEmail
+            // 
+            msgEmail.AutoSize = true;
+            msgEmail.ForeColor = Color.Red;
+            msgEmail.Location = new Point(413, 279);
+            msgEmail.Name = "msgEmail";
+            msgEmail.Size = new Size(30, 15);
+            msgEmail.TabIndex = 23;
+            msgEmail.Text = "msg";
+            // 
+            // msgTelephone
+            // 
+            msgTelephone.AutoSize = true;
+            msgTelephone.ForeColor = Color.Red;
+            msgTelephone.Location = new Point(32, 279);
+            msgTelephone.Name = "msgTelephone";
+            msgTelephone.Size = new Size(30, 15);
+            msgTelephone.TabIndex = 22;
+            msgTelephone.Text = "msg";
+            // 
+            // msgVille
+            // 
+            msgVille.AutoSize = true;
+            msgVille.ForeColor = Color.Red;
+            msgVille.Location = new Point(32, 207);
+            msgVille.Name = "msgVille";
+            msgVille.Size = new Size(30, 15);
+            msgVille.TabIndex = 21;
+            msgVille.Text = "msg";
+            // 
+            // msgRue
+            // 
+            msgRue.AutoSize = true;
+            msgRue.ForeColor = Color.Red;
+            msgRue.Location = new Point(31, 131);
+            msgRue.Name = "msgRue";
+            msgRue.Size = new Size(30, 15);
+            msgRue.TabIndex = 20;
+            msgRue.Text = "msg";
+            // 
+            // msgPrenom
+            // 
+            msgPrenom.AutoSize = true;
+            msgPrenom.ForeColor = Color.Red;
+            msgPrenom.Location = new Point(413, 62);
+            msgPrenom.Name = "msgPrenom";
+            msgPrenom.Size = new Size(30, 15);
+            msgPrenom.TabIndex = 19;
+            msgPrenom.Text = "msg";
+            // 
+            // msgNom
+            // 
+            msgNom.AutoSize = true;
+            msgNom.ForeColor = Color.Red;
+            msgNom.Location = new Point(32, 62);
+            msgNom.Name = "msgNom";
+            msgNom.Size = new Size(30, 15);
+            msgNom.TabIndex = 18;
+            msgNom.Text = "msg";
+            // 
+            // BtnAjouter
+            // 
+            BtnAjouter.BackColor = Color.Red;
+            BtnAjouter.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnAjouter.ForeColor = Color.Black;
+            BtnAjouter.Location = new Point(346, 382);
+            BtnAjouter.Name = "BtnAjouter";
+            BtnAjouter.Size = new Size(133, 50);
+            BtnAjouter.TabIndex = 17;
+            BtnAjouter.Text = "Ajouter";
+            BtnAjouter.UseVisualStyleBackColor = false;
+            // 
+            // Specialite
+            // 
+            Specialite.AutoSize = true;
+            Specialite.Location = new Point(413, 314);
+            Specialite.Name = "Specialite";
+            Specialite.Size = new Size(57, 15);
+            Specialite.TabIndex = 16;
+            Specialite.Text = "Spécialité";
+            // 
+            // cbxType
+            // 
+            cbxType.FormattingEnabled = true;
+            cbxType.Location = new Point(31, 332);
+            cbxType.Name = "cbxType";
+            cbxType.Size = new Size(248, 23);
+            cbxType.TabIndex = 15;
+            // 
+            // cbxSpecialite
+            // 
+            cbxSpecialite.FormattingEnabled = true;
+            cbxSpecialite.Location = new Point(413, 332);
+            cbxSpecialite.Name = "cbxSpecialite";
+            cbxSpecialite.Size = new Size(382, 23);
+            cbxSpecialite.TabIndex = 14;
+            // 
+            // Telephone
+            // 
+            Telephone.AutoSize = true;
+            Telephone.Location = new Point(31, 235);
+            Telephone.Name = "Telephone";
+            Telephone.Size = new Size(61, 15);
+            Telephone.TabIndex = 13;
+            Telephone.Text = "Téléphone";
+            // 
+            // mtbTelephone
+            // 
+            mtbTelephone.Location = new Point(31, 253);
+            mtbTelephone.Mask = "00 00 00 00 00 00";
+            mtbTelephone.Name = "mtbTelephone";
+            mtbTelephone.Size = new Size(120, 23);
+            mtbTelephone.TabIndex = 12;
+            // 
+            // Type
+            // 
+            Type.AutoSize = true;
+            Type.Location = new Point(31, 314);
+            Type.Name = "Type";
+            Type.Size = new Size(31, 15);
+            Type.TabIndex = 11;
+            Type.Text = "Type";
+            // 
+            // Email
+            // 
+            Email.AutoSize = true;
+            Email.Location = new Point(413, 235);
+            Email.Name = "Email";
+            Email.Size = new Size(36, 15);
+            Email.TabIndex = 10;
+            Email.Text = "Email";
+            // 
+            // Ville
+            // 
+            Ville.AutoSize = true;
+            Ville.Location = new Point(31, 163);
+            Ville.Name = "Ville";
+            Ville.Size = new Size(29, 15);
+            Ville.TabIndex = 9;
+            Ville.Text = "Ville";
+            // 
+            // Rue
+            // 
+            Rue.AutoSize = true;
+            Rue.Location = new Point(31, 87);
+            Rue.Name = "Rue";
+            Rue.Size = new Size(27, 15);
+            Rue.TabIndex = 8;
+            Rue.Text = "Rue";
+            // 
+            // Prenom
+            // 
+            Prenom.AutoSize = true;
+            Prenom.Location = new Point(413, 9);
+            Prenom.Name = "Prenom";
+            Prenom.Size = new Size(49, 15);
+            Prenom.TabIndex = 7;
+            Prenom.Text = "Prénom";
+            // 
+            // Nom
+            // 
+            Nom.AutoSize = true;
+            Nom.Location = new Point(32, 9);
+            Nom.Name = "Nom";
+            Nom.Size = new Size(34, 15);
+            Nom.TabIndex = 6;
+            Nom.Text = "Nom";
+            // 
+            // txtPrenom
+            // 
+            txtPrenom.Location = new Point(413, 36);
+            txtPrenom.Name = "txtPrenom";
+            txtPrenom.Size = new Size(357, 23);
+            txtPrenom.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(413, 253);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(382, 23);
+            txtEmail.TabIndex = 3;
+            // 
+            // txtVille
+            // 
+            txtVille.Location = new Point(31, 181);
+            txtVille.Name = "txtVille";
+            txtVille.Size = new Size(679, 23);
+            txtVille.TabIndex = 2;
+            // 
+            // txtRue
+            // 
+            txtRue.Location = new Point(31, 105);
+            txtRue.Name = "txtRue";
+            txtRue.Size = new Size(764, 23);
+            txtRue.TabIndex = 1;
+            // 
+            // txtNom
+            // 
+            txtNom.Location = new Point(31, 36);
+            txtNom.Name = "txtNom";
+            txtNom.Size = new Size(248, 23);
+            txtNom.TabIndex = 0;
+            // 
+            // FrmPraticienAjout
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(838, 585);
+            Controls.Add(panelCentral);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            Location = new Point(0, 0);
+            Name = "FrmPraticienAjout";
+            Text = "FrmPraticienAjout";
+            Controls.SetChildIndex(lblTitre, 0);
+            Controls.SetChildIndex(panelCentral, 0);
+            panelCentral.ResumeLayout(false);
+            panelCentral.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panelCentral;
+
+        private void centrerFormulaire()
+        {
+            panelCentral.Left = (this.ClientSize.Width - panelCentral.Width) / 2;
+            panelCentral.Top = lblTitre.Bottom;
+        }
+
+        private TextBox txtPrenom;
+        private TextBox txtEmail;
+        private TextBox txtVille;
+        private TextBox txtRue;
+        private TextBox txtNom;
+        private Label Ville;
+        private Label Rue;
+        private Label Prenom;
+        private Label Nom;
+        private Label Type;
+        private Label Email;
+        private MaskedTextBox mtbTelephone;
+        private Button BtnAjouter;
+        private Label Specialite;
+        private ComboBox cbxType;
+        private ComboBox cbxSpecialite;
+        private Label Telephone;
+        private Label msgSpecialite;
+        private Label msgType;
+        private Label msgEmail;
+        private Label msgTelephone;
+        private Label msgVille;
+        private Label msgRue;
+        private Label msgPrenom;
+        private Label msgNom;
+    }
+}
---
+++ Interface/FrmPraticienAjout.cs
@@ -0,0 +1,230 @@
+﻿using Donnee;
+using Metier;
+using System.Text.RegularExpressions;
+
+namespace Interface
+{
+    public partial class FrmPraticienAjout : FrmBase
+    {
+        public FrmPraticienAjout(Session uneSession) : base(uneSession)
+        {
+            InitializeComponent();
+            Load += FrmPraticienAjout_Load;
+        }
+
+        private void FrmPraticienAjout_Load(object sender, EventArgs e)
+        {
+            parametrerComposant();
+            centrerFormulaire();
+            this.Resize += FrmPraticienAjout_Resize;
+        }
+
+        private void parametrerComposant()
+        {
+            lblTitre.Text = "Ajouter un praticien";
+
+            msgNom.Visible = false;
+            msgPrenom.Visible = false;
+            msgRue.Visible = false;
+            msgVille.Visible = false;
+            msgTelephone.Visible = false;
+            msgEmail.Visible = false;
+            msgType.Visible = false;
+            msgSpecialite.Visible = false;
+
+            cbxType.DataSource = session.LesTypesPraticien;
+            cbxType.DisplayMember = "Libelle";
+            cbxType.ValueMember = "Id";
+            cbxType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxType.SelectedIndex = -1;
+
+            cbxSpecialite.DataSource = session.LesSpecialites;
+            cbxSpecialite.DisplayMember = "Libelle";
+            cbxSpecialite.ValueMember = "Id";
+            cbxSpecialite.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxSpecialite.SelectedIndex = -1;
+
+            txtVille.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            txtVille.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            var source = new AutoCompleteStringCollection();
+            foreach (Ville ville in session.MesVilles)
+            {
+                source.Add(ville.Nom);
+            }
+            txtVille.AutoCompleteCustomSource = source;
+
+            BtnAjouter.Click -= BtnAjouter_Click;
+            BtnAjouter.Click += BtnAjouter_Click;
+        }
+
+        private bool controlerChamp(TextBox txt, Label lblMessage, string message)
+        {
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                lblMessage.Text = message;
+                lblMessage.Visible = true;
+                return false;
+            }
+
+            lblMessage.Text = string.Empty;
+            lblMessage.Visible = false;
+            return true;
+        }
+
+        private bool controlerVille()
+        {
+            if (string.IsNullOrWhiteSpace(txtVille.Text))
+            {
+                msgVille.Text = "La ville du praticien doit être précisée";
+                msgVille.Visible = true;
+                return false;
+            }
+
+            bool existe = session.MesVilles.Any(v => string.Equals(v.Nom, txtVille.Text, StringComparison.OrdinalIgnoreCase));
+            if (!existe)
+            {
+                msgVille.Text = "La ville doit appartenir à la liste proposée";
+                msgVille.Visible = true;
+                return false;
+            }
+
+            msgVille.Text = string.Empty;
+            msgVille.Visible = false;
+            return true;
+        }
+
+        private bool controlerTelephone()
+        {
+            if (!mtbTelephone.MaskFull)
+            {
+                msgTelephone.Text = "Le téléphone du praticien doit être précisé";
+                msgTelephone.Visible = true;
+                return false;
+            }
+
+            msgTelephone.Visible = false;
+            return true;
+        }
+
+        private bool controlerEmail()
+        {
+            if (txtEmail.Text == string.Empty)
+            {
+                msgEmail.Text = "L'adresse mail du praticien doit être précisée";
+                msgEmail.Visible = true;
+                return false;
+            }
+
+            Regex uneExpression = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            if (!uneExpression.IsMatch(txtEmail.Text))
+            {
+                msgEmail.Text = "L'adresse mail n'est pas valide";
+                msgEmail.Visible = true;
+                return false;
+            }
+
+            msgEmail.Text = string.Empty;
+            msgEmail.Visible = false;
+            return true;
+        }
+
+        private bool controlerType()
+        {
+            if (cbxType.SelectedIndex == -1)
+            {
+                msgType.Text = "Veuillez sélectionner le type de praticien.";
+                msgType.Visible = true;
+                return false;
+            }
+
+            msgType.Text = string.Empty;
+            msgType.Visible = false;
+            return true;
+        }
+
+        private void ajout()
+        {
+            bool nomOk = controlerChamp(txtNom, msgNom, "Le nom du praticien doit être précisé");
+            bool prenomOk = controlerChamp(txtPrenom, msgPrenom, "Le prénom du praticien doit être précisé");
+            bool rueOk = controlerChamp(txtRue, msgRue, "La rue du praticien doit être précisée");
+            bool villeOk = controlerVille();
+            bool emailOk = controlerEmail();
+            bool telephoneOk = controlerTelephone();
+            bool typeOk = controlerType();
+
+            if (nomOk && prenomOk && rueOk && villeOk && emailOk && telephoneOk && typeOk)
+            {
+                ajouter();
+            }
+        }
+
+        private void ajouter()
+        {
+            try
+            {
+                Ville laVille = session.MesVilles.Find(x => string.Equals(x.Nom, txtVille.Text, StringComparison.OrdinalIgnoreCase))!;
+                string codePostal = laVille.Code;
+
+                string idType = ((TypePraticien)cbxType.SelectedItem!).Id;
+                string? idSpecialite = cbxSpecialite.SelectedIndex >= 0 ? ((Specialite)cbxSpecialite.SelectedItem!).Id : null;
+
+                int id = Passerelle.ajouterPraticien(
+                    txtNom.Text.Trim(),
+                    txtPrenom.Text.Trim(),
+                    txtRue.Text.Trim(),
+                    codePostal,
+                    laVille.Nom,
+                    mtbTelephone.Text,
+                    txtEmail.Text.Trim(),
+                    idType,
+                    idSpecialite);
+
+                TypePraticien leType = session.LesTypesPraticien.Find(x => x.Id == idType)!;
+                Specialite? laSpecialite = idSpecialite is null ? null : session.LesSpecialites.Find(x => x.Id == idSpecialite);
+
+                var praticien = new Praticien(
+                    id,
+                    txtNom.Text.Trim(),
+                    txtPrenom.Text.Trim(),
+                    txtRue.Text.Trim(),
+                    codePostal,
+                    laVille.Nom,
+                    txtEmail.Text.Trim(),
+                    mtbTelephone.Text,
+                    leType,
+                    laSpecialite);
+
+                session.MesPraticiens.Add(praticien);
+                session.MesPraticiens.Sort();
+
+                txtNom.Clear();
+                txtPrenom.Clear();
+                txtRue.Clear();
+                txtVille.Clear();
+                txtEmail.Clear();
+                mtbTelephone.Clear();
+                cbxType.SelectedIndex = -1;
+                cbxSpecialite.SelectedIndex = -1;
+
+                MessageBox.Show("Le praticien a été ajouté avec succès.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnAjouter_Click(object? sender, EventArgs e)
+        {
+            ajout();
+        }
+
+        private void FrmPraticienAjout_Resize(object? sender, EventArgs e)
+        {
+            centrerFormulaire();
+        }
+
+
+
+    }
+}
